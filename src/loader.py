fde755eb388de3c33450ffbfc7cabab7c64c29ca
Amanell <nikitaa.lebedintsev@gmail.com>
2025-05-28 22:51:36 +0200

    loader fix

--- src/loader.py
+++ src/loader.py
@@ -1,29 +1,29 @@
 from typing import Any, Dict, Optional
 import pandas as pd
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 
-
 class ImageDataset(Dataset):
-    def __init__(self, df: pd.DataFrame, transform: Optional[Any] = None):
-        self.data = df
+    def __init__(self, dataframe: pd.DataFrame, transform: Optional[Any] = None):
+        self.df = dataframe
         self.transform = transform or transforms.Compose([
             transforms.RandomHorizontalFlip(),
             transforms.RandomRotation(15),
+            transforms.ToTensor(),
         ])
 
-    def __len__(self):
-        return len(self.data)
+    def __len__(self): return len(self.df)
 
-    def __getitem__(self, index):
-        image = self.data.iloc[index]["image"]
-        label = self.data.iloc[index]["label"]
+    def __getitem__(self, idx):
+        image = torch.tensor(self.df.iloc[idx]["image"], dtype=torch.float32)
+        label = self.df.iloc[idx]["label"]
+        return self.transform(image), label
 
-        # image is already a tensor (float32), skip ToTensor
-        image = torch.tensor(image, dtype=torch.float32)
-
-        if self.transform:
-            image = self.transform(image)
-
-        return image, label
+def create_data_loader(df: pd.DataFrame, config: Dict[str, Any]) -> DataLoader:
+    return DataLoader(
+        ImageDataset(df, config.get("transform")),
+        batch_size=config.get("batch_size", 32),
+        shuffle=True,
+        num_workers=config.get("num_workers", 2)
+    )
